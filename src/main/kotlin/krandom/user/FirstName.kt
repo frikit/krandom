4d0b393d554656c99946b55c32a9e28545351be2
frikiT <osipovvictor1994@gmail.com>
2019-06-30 13:39:13 +0300

    make user random code more generic for easy future implimentation

--- src/main/kotlin/krandom/user/FirstName.kt
+++ src/main/kotlin/krandom/user/FirstName.kt
@@ -1,7 +1,6 @@
 package krandom.user
 
 import krandom.KRandomUser
-import krandom.exceptions.SizeLimitExceedException
 import krandom.user.common.GenericUserGenerator
 import krandom.utils.ResourcePathHolder
 
@@ -11,18 +10,15 @@ class FirstName : KRandomUser<String>, GenericUserGenerator() {
     private val list by lazy { initCache(ResourcePathHolder.relativeFirstNamePath) }
 
     override fun randomData(): String {
-        val list = randomDatas()
-        val index = kRandomCommon.randomInt(0, list.size - 1)
-        return list[index]
+        return randomData(list)
     }
 
     override fun randomDatas(): List<String> {
         val size = kRandomCommon.randomInt(2..maxAllowSize)
-        return randomDatas(size)
+        return randomDatas(list, size)
     }
 
     override fun randomDatas(size: Int): List<String> {
-        if (size > maxAllowSize) SizeLimitExceedException("Size cannot be > $maxAllowSize!")
-        return list.shuffled().take(size).toList()
+        return randomDatas(list, size)
     }
 }
--- src/main/kotlin/krandom/user/common/GenericUserGenerator.kt
+++ src/main/kotlin/krandom/user/common/GenericUserGenerator.kt
@@ -2,6 +2,7 @@ package krandom.user.common
 
 import krandom.common.KRandomCommon
 import krandom.common.Randomizer
+import krandom.exceptions.SizeLimitExceedException
 import krandom.utils.CSVParser
 import krandom.utils.ResourceResolver
 
@@ -9,12 +10,23 @@ abstract class GenericUserGenerator {
     val kRandomCommon: KRandomCommon by lazy { Randomizer() }
     open var maxAllowSize = 10_000
 
-    fun initCache(relativePath: String): List<String> {
+    protected fun initCache(relativePath: String): List<String> {
         val content = ResourceResolver.getResourceContent(relativePath)
         val list = CSVParser.parse(content, CSVParser.csvDelimiter).requireNoNulls()
         validateList(list)
 
         return list
+    }
+
+    protected fun randomData(list: List<String>): String {
+        val index = kRandomCommon.randomInt(0, list.size - 1)
+        return list[index]
+    }
+
+    protected fun randomDatas(list: List<String>, size: Int): List<String> {
+        if (size > maxAllowSize) throw SizeLimitExceedException("Size cannot be > $maxAllowSize!")
+        if (size < 0) throw SizeLimitExceedException("Size cannot be < 0!")
+        return list.shuffled().take(size).toList()
     }
 
     private fun validateList(list: List<String>) {
