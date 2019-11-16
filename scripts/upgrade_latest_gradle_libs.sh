e8fcd25c0d38f2b51d8ba081ba951cafa9a97693
frikit <osipovvictor1994@gmail.com>
2019-11-16 14:59:11 +0000

    create auto upgrade script for jacoco and kotlin libs version based on the lastes avaialble in repo

---
+++ scripts/upgrade_latest_gradle_libs.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+curr=${PDW}
+cd ../
+
+latest="latest"
+
+echo Update jacoco lib
+url="https://mvnrepository.com/artifact/org.jacoco/org.jacoco.agent/"
+
+echo Follow redirects to find version...
+redirectUrl=$(curl -Lis -o /dev/null -w %{url_effective} ${url}${latest})
+
+echo "Find url [$redirectUrl]"
+versionLib=${redirectUrl/$url/""}
+echo "Find version [$versionLib]"
+
+sed -i.back "s/\(^jacoco_version=\).*/\1$versionLib/g" gradle.properties
+
+echo Update kotlin lib
+url="https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib/"
+
+echo Follow redirects to find version...
+redirectUrl=$(curl -Lis -o /dev/null -w %{url_effective} ${url}${latest})
+
+echo "Find url [$redirectUrl]"
+versionLib=${redirectUrl/$url/""}
+echo "Find version [$versionLib]"
+
+sed -i.back "s/\(^kotlin_version=\).*/\1$versionLib/g" gradle.properties
+
+echo Remove whitespaces...
+cat gradle.properties | tr -d "[:blank:]" > gradle.properties.back
+
+echo Update current gradle.properties...
+mv -vf gradle.properties.back gradle.properties
+
+echo Output gradle.properties...
+cat gradle.properties
+
+cd ${curr}
