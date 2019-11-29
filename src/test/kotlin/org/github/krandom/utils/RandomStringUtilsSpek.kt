275f6f5665a663ccad497090132289d864f15b4e
frikiT <osipovvictor1994@gmail.com>
2019-11-29 17:18:50 +0000

    increase code coverage for random string utils

--- src/test/kotlin/org/github/krandom/utils/RandomStringUtilsSpek.kt
+++ src/test/kotlin/org/github/krandom/utils/RandomStringUtilsSpek.kt
@@ -77,6 +77,32 @@ object RandomStringUtilsSpek : Spek({
                 assert(res.length > 1)
             }
         }
+
+        describe("test if start=0 and end=0 not null chars") {
+            val res = random(count = 2, start = 0, end = 0, letters = false, numbers = false, chars = null)
+            it("[$res] should not be blank") {
+                assert(res.isNotBlank()) { "[$res] is blank!" }
+            }
+            it("[$res] should not be empty") {
+                assert(res.isNotEmpty()) { "[$res] is empty!" }
+            }
+            it("[$res] should be valid size") {
+                assert(res.length > 1)
+            }
+        }
+
+        describe("test if start=0 and end=0 not null chars") {
+            val res = random(count = 2, start = 0, end = 0, letters = false, numbers = true, chars = null)
+            it("[$res] should not be blank") {
+                assert(res.isNotBlank()) { "[$res] is blank!" }
+            }
+            it("[$res] should not be empty") {
+                assert(res.isNotEmpty()) { "[$res] is empty!" }
+            }
+            it("[$res] should be valid size") {
+                assert(res.length > 1)
+            }
+        }
     }
 
     describe("test if exception is thrown") {
